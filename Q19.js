49e8bbf47405504fe0eed86c59a18332bd66f403
srinalini2004 <160092260+srinalini2004@users.noreply.github.com>
2025-11-26 22:46:53 +0530

    Create Q19.js

---
+++ Q19.js
@@ -0,0 +1,14 @@
+let age = 25;
+
+function displayAge() {
+    console.log("Current Age:", age);
+}
+
+function changeAge() {
+    age = 30;
+    console.log("Age Updated Inside changeAge:", age);
+}
+
+displayAge();
+changeAge();
+displayAge();
